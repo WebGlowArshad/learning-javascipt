6a5b5751d70a121c824ba87be0ffa4e831efdf4c
WebGlowArshad <arshadkhan121912@gmail.com>
2024-09-25 06:11:22 +0000

    conversation operators

---
+++ 1 basics/04 conversation operator.js
@@ -0,0 +1,25 @@
+let score = "4545jhu"
+console.log(typeof score);
+console.log(typeof(score));
+ let valueInnumber = Number(score)
+ console.log(score);
+ console.log( typeof valueInnumber);
+ console.log(valueInnumber);
+
+ let IsloggedIn = "arshad"
+ let booleanIsloggedIn = Boolean(IsloggedIn)
+ console.log(booleanIsloggedIn);
+ 
+ 
+ 
+ 
+// 1= true   / 0= false
+// "" = false
+//"arshad" = true
+
+
+let somenumber = 55
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
