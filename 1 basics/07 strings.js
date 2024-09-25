29c65c49fd625eacbe21b3a3bbe41df08b26188b
WebGlowArshad <arshadkhan121912@gmail.com>
2024-09-25 13:38:29 +0000

    strings

---
+++ 1 basics/07 strings.js
@@ -0,0 +1,42 @@
+// const name = 'arshad khan'
+// const repocount = 50
+
+// // console.log('my name is ${name}and my repocoutn is ${repocount}');
+// // console.log(`my name is ${name}and my repocount is &{repocount}`);
+// console.log(`my name is ${name} and my repocout is ${repocount}`);
+
+// let name = 'arshad khan'
+// let age = 22
+// console.log(`my name is ${name} and my age is ${age}`);
+
+
+// let babyname = "Arshadkhan"
+// console.log(babyname.charAt);
+// console.log(babyname.concat);
+// console.log(babyname.length);
+// console.log(babyname.at);
+// console.log(babyname.codePointAt);
+// console.log(babyname.endsWith(5));
+// console.log(babyname.indexOf(5));
+// console.log(babyname.slice(2));
+
+ const newString = '      arshad     '
+ console.log(newString);
+ console.log(newString.trim());
+ console.log(newString);
+const url = 'https://www.webglowarshad.com/20%arshad'
+
+ console.log(url.replace('%','_'));
+console.log(url.replace('webglowarshad','kingkhan'))
+console.log(url.includes('arshad'));
+
+
+
+
+
+
+
+
+
+
+
