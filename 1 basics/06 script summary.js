15e0f62dac146d4e4ecbbc957eea28af4a10944a
WebGlowArshad <arshadkhan121912@gmail.com>
2024-09-25 09:05:27 +0000

    script summary

---
+++ 1 basics/06 script summary.js
@@ -0,0 +1,37 @@
+/* 
+***************Primitive datatypes****************
+String = "arshad khan"
+Number = 9918019406
+boolean = true / false
+undefined =
+symbol =
+null
+
+
+********************Non Primitive datatypes ****************
+1=Array,  2=Object 3= Function
+*/
+ let arr = [ "arshad" +"khan" + 9918019406]
+ console.log(arr);
+
+
+let obj ={
+    name:"arshad",
+    class: "b.com",
+    subject: "maths"
+
+
+
+
+}
+console.log(obj);
+
+
+function khan(params) {
+    console.log(khan);
+    
+}
+
+
+
+ 
