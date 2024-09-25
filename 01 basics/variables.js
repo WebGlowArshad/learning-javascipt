ab02804300e7e42e3c6a04ecf2291efc44cb975b
WebGlowArshad <arshadkhan121912@gmail.com>
2024-09-25 05:27:59 +0000

    variables

---
+++ 01 basics/variables.js
@@ -0,0 +1,15 @@
+const accountID = 45465665
+let accountgmail = "arshadkhan121912@gmail.com"
+var accountpassword = "Arshad khan"
+accountcity = "deoria"
+
+// accountID = 2 (not allowed because when u create a const varibale it has naver change)
+console.log(accountID);
+
+accountgmail="khan@gmail.com"
+accountpassword = "webglowarshad"
+ accountcity = " gorakhpur"
+let accountstate;
+
+console.table([accountID,accountgmail,accountpassword,accountcity,accountstate])
+
