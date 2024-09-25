973002a12415aa807df8a6bf23f30097f65fd98d
WebGlowArshad <arshadkhan121912@gmail.com>
2024-09-25 05:28:19 +0000

    datatypes

---
+++ 01 basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all js code as newer version
+let name = "arshad khan"
+let age = 18
+console.log(3+3);
+console.log("arshad khan"); // this is not good practice//
+
+// number = 1245654
+// string = "arshad khan"
+// boolean = true or false
+//bigint = 
+// mull =
+// undefined
+// symbol
+
+console.log(typeof name);
+console.log(typeof age);
