c1ec83773c06bbadbb601b49369ed66bb00e4482
WebGlowArshad <arshadkhan121912@gmail.com>
2024-09-25 07:28:27 +0000

    conversation

--- 1 basics/04 conversation operator.js
+++ 1 basics/04 conversation operator.js
@@ -1,25 +1,39 @@
-let score = "4545jhu"
+let score = "4545jhu";
 console.log(typeof score);
-console.log(typeof(score));
- let valueInnumber = Number(score)
- console.log(score);
- console.log( typeof valueInnumber);
- console.log(valueInnumber);
+console.log(typeof score);
+let valueInnumber = Number(score);
+console.log(score);
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
 
- let IsloggedIn = "arshad"
- let booleanIsloggedIn = Boolean(IsloggedIn)
- console.log(booleanIsloggedIn);
- 
- 
- 
- 
+let IsloggedIn = "arshad";
+let booleanIsloggedIn = Boolean(IsloggedIn);
+console.log(booleanIsloggedIn);
+
 // 1= true   / 0= false
 // "" = false
 //"arshad" = true
 
-
-let somenumber = 55
-let stringnumber = String(somenumber)
+let somenumber = 55;
+let stringnumber = String(somenumber);
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+//*****************Operators**************** */
+let str1 = "arhad";
+let str2 = " khan";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(5 + 5);
+console.log("5" + 5);
+console.log(5 + 5 + '10');
+console.log((5+5)+10);
+console.log((5+5)+(20/2));
+console.log((5-5)+(20/2));
+
+
+
+
+
 
